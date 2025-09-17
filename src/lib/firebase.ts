ba92dc74b56415da743d9da8238f66b9f78a7546
MAYANK RAJ <mayankrajdto@gmail.com>
2025-09-17 21:01:57 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
@@ -5,12 +6,12 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: "studio-6167596920-dbcef",
-  appId: "1:1041595331915:web:58281b2d730cd78670a4ce",
-  storageBucket: "studio-6167596920-dbcef.firebasestorage.app",
-  apiKey: "AIzaSyAGbbOra2xq5gofQglFln0AWKzYZLJSHNk",
-  authDomain: "studio-6167596920-dbcef.firebaseapp.com",
-  messagingSenderId: "1041595331915",
+  "projectId": "studio-6167596920-dbcef",
+  "appId": "1:1041595331915:web:58281b2d730cd78670a4ce",
+  "storageBucket": "studio-6167596920-dbcef.firebasestorage.app",
+  "apiKey": "AIzaSyAGbbOra2xq5gofQglFln0AWKzYZLJSHNk",
+  "authDomain": "studio-6167596920-dbcef.firebaseapp.com",
+  "messagingSenderId": "1041595331915"
 };
 
 // Initialize Firebase
